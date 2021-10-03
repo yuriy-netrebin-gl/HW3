e9b8fc6d436ed57591f222842e0dabb189b34791
yuriy.netrebin <yuriy.netrebin@globallogic.com>
2021-10-04 00:45:26 +0300

    added compute top 10

--- src/main/java/KafkaConsumerTopTenBitcoin.java
+++ src/main/java/KafkaConsumerTopTenBitcoin.java
@@ -5,10 +5,12 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.json.JSONObject;
 
 import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class KafkaConsumerTopTenBitcoin {
     private final static String TOPIC = "readBitcoinsTransactions";
@@ -17,10 +19,11 @@ public class KafkaConsumerTopTenBitcoin {
     private static Consumer<Long, String> createConsumer() {
         final Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaLabGroup");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaLabGroup" + new Date().getTime());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // Create the consumer using props.
         final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
@@ -44,11 +47,18 @@ public class KafkaConsumerTopTenBitcoin {
                 if (noRecordsCount > giveUp) break;
                 else continue;
             }
-
-            consumerRecords.forEach(record -> System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
-                    record.key(), record.value(),
-                    record.partition(), record.offset()));
-
+            List<JSONObject> list = new ArrayList<>();
+            consumerRecords.forEach(record -> list.add(new JSONObject(record.value())));
+            System.out.println("***** Top 10 bitcoin transactions based on price field (descending) *****");
+            List<JSONObject> filteredList = list.stream()
+                    .filter(r -> !r.isEmpty() && !r.isNull("data") && !r.getJSONObject("data").isEmpty())
+                    .sorted(Comparator.comparing(r -> r.getJSONObject("data").getDouble("price")))
+                    .collect(Collectors.toList());
+            Collections.reverse(filteredList);
+            AtomicInteger i = new AtomicInteger(1);
+            filteredList.stream()
+                    .limit(10)
+                    .forEach(r -> System.out.printf("%d. %s%n", i.getAndIncrement(), r));
             consumer.commitAsync();
         }
         System.out.println("DONE");
